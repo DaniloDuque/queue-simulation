62f32bcdc6205a83a89a79402a3fc9878e1934bc
K-lobiTo <calebam2112@gmail.com>
2026-01-16 17:07:12 -0600

    add stationSequence to client

--- src/main/java/org/sim/model/Client.java
+++ src/main/java/org/sim/model/Client.java
@@ -1,5 +1,9 @@
 package org.sim.model;
 
+import java.util.Collection;
+
+import org.sim.station.ServiceStation;
+
 import lombok.Getter;
 import lombok.Setter;
 import lombok.RequiredArgsConstructor;
@@ -10,6 +14,9 @@ public class Client {
 
     private final long id;
     private final double arrivalTime;
+
+    @Setter
+    private Collection<ServiceStation> stationSequence;
 
     @Setter
     private double totalWaitingTime = 0.0;
