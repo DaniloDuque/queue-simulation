4a302e3c01126cf526ce0726e4ca5f8f1205e8a8
DaniloDuque <dduque@estudiantec.cr>
2026-01-16 10:56:34 -0600

    add guice

---
+++ src/main/java/org/sim/module/SimulationModule.java
@@ -0,0 +1,10 @@
+package org.sim.module;
+
+import com.google.inject.AbstractModule;
+
+public class SimulationModule extends AbstractModule {
+
+    @Override
+    protected void configure() {
+    }
+}
