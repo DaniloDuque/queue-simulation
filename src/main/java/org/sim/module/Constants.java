6b9decce7a0bfad0ab720e4efb83ca5ac0c4a0fb
K-lobiTo <calebam2112@gmail.com>
2026-01-16 16:55:58 -0600

    add Station Router

---
+++ src/main/java/org/sim/module/Constants.java
@@ -0,0 +1,10 @@
+package org.sim.module;
+
+public class Constants {
+
+    public static final double CASHIER_PROB = 1;
+    public static final double DRINKS_PROB = 0.9;
+    public static final double FRIER_PROB = 0.7;
+    public static final double DESERT_PROB = 0.25;
+    public static final double CHICKEN_PROB = 0.3;
+}
--- src/main/java/org/sim/station/StationRouter.java
+++ src/main/java/org/sim/station/StationRouter.java
@@ -1,4 +1,41 @@
 package org.sim.station;
 
+import com.google.inject.Inject;
+import lombok.AllArgsConstructor;
+import java.util.ArrayList;
+import java.util.Collection;
+import java.util.List;
+
+import org.sim.module.Constants;
+
+@AllArgsConstructor(onConstructor_ = @Inject)
 public class StationRouter {
+
+    private final ServiceStation cashier;
+    private final ServiceStation drinks;
+    private final ServiceStation frier;
+    private final ServiceStation desert;
+    private final ServiceStation chicken;
+
+    public Collection<ServiceStation> getStationSequence() {
+        final List<ServiceStation> stationSequence = new ArrayList<>();
+
+        if (Math.random() < Constants.CASHIER_PROB) {
+            stationSequence.add(cashier);
+        }
+        if (Math.random() < Constants.DRINKS_PROB) {
+            stationSequence.add(drinks);
+        }
+        if (Math.random() < Constants.FRIER_PROB) {
+            stationSequence.add(frier);
+        }
+        if (Math.random() < Constants.DESERT_PROB) {
+            stationSequence.add(desert);
+        }
+        if (Math.random() < Constants.CHICKEN_PROB) {
+            stationSequence.add(chicken);
+        }
+
+        return stationSequence;
+    }
 }
